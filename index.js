3e2058df85f190dfde132e0c1f2aab3f44be6062
tashimtishan <tashimtishan01611412720@gmail.com>
2026-06-23 15:45:49 +0600

    implemented booking feature api

--- index.js
+++ index.js
@@ -1,13 +1,70 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+dotenv.config();
 const app = express();
-const PORT= process.env.PORT || 8000
+app.use(express.json());
+const PORT = process.env.PORT
+const cors = require("cors");
+app.use(cors());
 
+const uri = process.env.MONGODB_URI;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("DocAppoint");
+    const doctorsCollection = db.collection("doctors");
+    const bookingCollection = db.collection("bookings");
+
+    app.get("/doctors", async (req, res) => {
+      const result = await doctorsCollection.find().toArray();
+      res.json(result);
+    })
+
+    app.get("/doctors/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await doctorsCollection.findOne({ _id: new ObjectId(id) });
+      res.json(result)
+    })
+
+
+    app.get("/bookings/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await bookingCollection.find({ userId: id }).toArray();
+      res.json(result);
+    });
+
+    app.get("/bookings", async (req, res) => {
+      const result = await bookingCollection.find().toArray();
+      res.json(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("server is running just okay!!!!")
+  res.send("server is running just okay!!!!")
 })
 
-app.listen(PORT,()=>{
-    console.log(`app is running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`app is running on port ${PORT}`)
 })
